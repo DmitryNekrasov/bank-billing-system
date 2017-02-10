6c4f2c97a5edcfa2576fe5d6a0ad7eb026be3c5d
Dmitry <scannorone@gmail.com>
2017-02-10 14:13:25 +0700

    Поправил ожидание сервера. Добавил start_client.

--- src/start_server.erl
+++ src/start_server.erl
@@ -3,10 +3,12 @@
 
 -export([main/0]).
 
+loop() -> loop().
+
 main() ->
   erlang:display(compile:file('src/bank')),
   erlang:display(compile:file('src/bank_server')),
   erlang:display(code:load_file(bank)),
   erlang:display(code:load_file(bank_server)),
   erlang:display(bank_server:start_link()),
-  timer:sleep(10000000).
+  loop().
