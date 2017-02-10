242169247b91a3bf0e9d25a49c4ae68636592d3a
Dmitry <scannorone@gmail.com>
2017-02-10 14:19:11 +0700

    Перенёс имя сервера в параметр функции. Добавил проверку доступности сервера.

--- src/start_client.erl
+++ src/start_client.erl
@@ -1,18 +1,23 @@
 -module(start_client).
 -author("nekrasov").
 
--export([main/0]).
+-export([main/1]).
 
-main() ->
-  erlang:display(net_kernel:connect_node('bank_server@razrab11.csort.local')),
-  erlang:display(code:load_file(bank_server)),
-  timer:sleep(1000),
-  erlang:display(bank_server:getBalance(64, 16)),
-  erlang:display(bank_server:putMoney(64, 16, 1000)),
-  erlang:display(bank_server:getBalance(64, 16)),
-  erlang:display(bank_server:getMoney(64, 16, 25)),
-  erlang:display(bank_server:getBalance(64, 16)),
-  erlang:display(bank_server:getBalance(32, 8)),
-  erlang:display(bank_server:sendMoney(64, 16, 32, 100)),
-  erlang:display(bank_server:getBalance(64, 16)),
-  erlang:display(bank_server:getBalance(32, 8)).
+main(ServerName) ->
+  Connect = net_kernel:connect_node(ServerName),
+  erlang:display(Connect),
+  if
+    Connect ->
+      erlang:display(code:load_file(bank_server)),
+      timer:sleep(1000),
+      erlang:display(bank_server:getBalance(64, 16)),
+      erlang:display(bank_server:putMoney(64, 16, 1000)),
+      erlang:display(bank_server:getBalance(64, 16)),
+      erlang:display(bank_server:getMoney(64, 16, 25)),
+      erlang:display(bank_server:getBalance(64, 16)),
+      erlang:display(bank_server:getBalance(32, 8)),
+      erlang:display(bank_server:sendMoney(64, 16, 32, 100)),
+      erlang:display(bank_server:getBalance(64, 16)),
+      erlang:display(bank_server:getBalance(32, 8));
+    true -> ok
+  end.
