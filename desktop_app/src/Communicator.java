d1ce936a5244070cf8c8d441451f62c911505591
Dmitry <scannorone@gmail.com>
2017-02-13 11:14:44 +0700

    Получение курса валют

--- desktop_app/src/Communicator.java
+++ desktop_app/src/Communicator.java
@@ -40,4 +40,33 @@ public class Communicator {
         return true;
     }
 
+    public int[] getCurrency() {
+        int[] res = new int[2];
+        res[0] = -1;
+
+        try {
+            connection.sendRPC("bank_server", "getCurrency", new OtpErlangList());
+        } catch (IOException e) {
+            e.printStackTrace();
+            return res;
+        }
+
+        OtpErlangTuple received;
+        try {
+            received = (OtpErlangTuple) connection.receiveRPC();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return res;
+        }
+
+        if (!received.toString().contains("badrpc")) {
+            res[0] = Integer.valueOf(received.elementAt(0).toString());
+            res[1] = Integer.valueOf(received.elementAt(1).toString());
+        } else {
+            System.err.println(received);
+        }
+
+        return res;
+    }
+
 }
--- desktop_app/src/Main.java
+++ desktop_app/src/Main.java
@@ -2,7 +2,12 @@ public class Main {
     public static void main(String[] args) {
         String serverNodeName = args[0];
         String selfNodeName = args[1];
+
         Communicator communicator = new Communicator(serverNodeName, selfNodeName);
         communicator.connectToServer();
+
+        int[] currency = communicator.getCurrency();
+        System.out.println("eur: " + currency[0]);
+        System.out.println("usd: " + currency[1]);
     }
 }
